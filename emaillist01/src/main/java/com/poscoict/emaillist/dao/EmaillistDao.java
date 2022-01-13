c53e8f4d98ca7596b0194d4cc1f60d4493058412
BIT <BIT@DESKTOP-7FO0C42>
2022-01-13 11:48:34 +0900

    guestbook01 #1

--- emaillist01/src/main/java/com/poscoict/emaillist/dao/EmaillistDao.java
+++ emaillist01/src/main/java/com/poscoict/emaillist/dao/EmaillistDao.java
@@ -19,12 +19,7 @@ public class EmaillistDao {
 		ResultSet rs = null;
 		
 		try {
-			//1. JDBC 드라이버 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//2. 연결하기
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");			
+			conn = getConnection();			
 		
 			//3. SQL 준비
 			String sql = "select no, first_name, last_name, email from emaillist order by no desc";
@@ -49,8 +44,6 @@ public class EmaillistDao {
 				
 				result.add(vo);
 			}
-		} catch (ClassNotFoundException e) {
-			System.out.print("드라이버 로딩 실패 : " + e);
 		} catch (SQLException e) {
 			System.out.print("error : " + e);
 		} finally {
@@ -81,12 +74,7 @@ public class EmaillistDao {
 		ResultSet rs = null;
 		
 		try {
-			//1. JDBC 드라이버 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//2. 연결하기
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");			
+			conn = getConnection();			
 		
 			//3. SQL 준비
 			String sql = "insert into emaillist values(null, ?, ?, ?)";
@@ -100,8 +88,6 @@ public class EmaillistDao {
 			//5. SQL 실행
 			result = pstmt.executeUpdate() == 1;
 		
-		} catch (ClassNotFoundException e) {
-			System.out.print("드라이버 로딩 실패 : " + e);
 		} catch (SQLException e) {
 			System.out.print("error : " + e);
 		} finally {
@@ -122,5 +108,22 @@ public class EmaillistDao {
 		}
 		
 		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			//1. JDBC 드라이버 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//2. 연결하기
+			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch(ClassNotFoundException e) {
+			System.out.print("드라이버 로딩 실패 :" + e);
+		} 
+		
+		return conn;
 	}
 }
