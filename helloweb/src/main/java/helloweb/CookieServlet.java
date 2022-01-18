99e2f5c21bea73a6503debab2501a4a2614f6b75
BIT <BIT@DESKTOP-7FO0C42>
2022-01-18 11:48:01 +0900

    cookie #1

---
+++ helloweb/src/main/java/helloweb/CookieServlet.java
@@ -0,0 +1,38 @@
+package helloweb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String COOKIE_NAME = "visitcount";
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int visitCount = 0;
+		
+		// 쿠키 읽기
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null && cookies.length > 0) {
+			for(Cookie cookie : cookies) {
+				if(COOKIE_NAME.equals(cookie.getName()) {
+					visitCount = Integer.parseInt(cookie.getValue());
+					break;
+				}
+			}
+		}
+		
+		visitCount++;
+		
+		// 쿠키쓰기
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
